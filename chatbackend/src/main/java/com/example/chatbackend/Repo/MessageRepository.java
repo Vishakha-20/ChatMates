f6427ca4c4e6bf26eef6da54474778564746e29c
Vishakha Dharmendra Bisen <nrl.vishaka.bisen@gmail.com>
2025-07-15 14:03:31 +0530

    Remove old chatbackend and frontend folders

--- chatbackend/src/main/java/com/example/chatbackend/Repo/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatbackend.Repo;
-
-import com.example.chatbackend.Models.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
--- frontend/lib/theme/app_color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color.fromARGB(255, 76, 97, 120);
-  static const background = Color(0xFFF5F7FA);
-  static const textDark = Color(0xFF2C3E50);
-  static const white = Colors.white;
-}
